7fc87f2109c587907ead3587000e3e4c6cc4a11b
Grise <griseldaremedi@gmail.com>
2022-10-26 19:25:42 -0300

    Updates

---
+++ precache-manifest.5287659e47809e7a5134cab682f7a967.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b657d69349275f4e331",
+    "url": "/appContador/static/css/main.603ee4fc.chunk.css"
+  },
+  {
+    "revision": "7b657d69349275f4e331",
+    "url": "/appContador/static/js/main.7b657d69.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/appContador/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/appContador/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "54ba35a9d593e494cc68",
+    "url": "/appContador/static/js/runtime~main.54ba35a9.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/appContador/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f4222a3c2cfd0c1b84c997a7aa9929ac",
+    "url": "/appContador/static/media/body_flores.f4222a3c.jpg"
+  },
+  {
+    "revision": "dadbbdf5d63c2875ffbbe6244e0f50ec",
+    "url": "/appContador/index.html"
+  }
+];
